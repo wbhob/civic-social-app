6483422994eba9aad8caadb81c1cb1e446107a60
wbhob <wilsonhobbs1@gmail.com>
2016-09-12 19:25:22 -0400

    F

--- app/index.php
+++ app/index.php
@@ -2,23 +2,23 @@
 <html>
 <head>
     <link href="assets/css/normalize.css" type="text/css" />
-        <link href="assets/css/skeleton.css" type="text/css" />
+    <link href="assets/css/skeleton.css" type="text/css" />
     <link href="assets/css/main.css" type="text/css" />
+    <title>CivicSocial</title>
 </head>
 <body>
   <?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
+      $servername = "civicsocialmysql.wilsonhobbs.com";
+      $username = "wilsonhobbs";
+      $password = "hobbs8";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
+      // Create connection
+      $conn = new mysqli($servername, $username, $password);
 
-// Check connection
-if ($conn->connect_error) {
-   die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
+      // Check connection
+      if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+      }
 ?>
 
 </body>
