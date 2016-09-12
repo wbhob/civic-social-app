f259f7e856b7a1f11f8175f8c0c1d8e1d7dac51f
wbhob <wilsonhobbs1@gmail.com>
2016-09-12 19:14:43 -0400

    Added some files

--- app/index.php
+++ app/index.php
@@ -1,1 +1,25 @@
 <!DOCTYPE html>
+<html>
+<head>
+    <link href="assets/css/normalize.css" type="text/css" />
+        <link href="assets/css/skeleton.css" type="text/css" />
+    <link href="assets/css/main.css" type="text/css" />
+</head>
+<body>
+  <?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+   die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
+
+</body>
+</html>
