5195ade8e34bc953bd836e4d0eb2252c1b2af300
wbhob <wilsonhobbs1@gmail.com>
2016-10-10 19:22:59 -0400

    I tried webpacking and failed epically...

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
+
+var whichPolygon = require("which-polygon");
 angular.module("civicsocial", ["firebase"])
-	.controller("AppCtrl", function($scope, $firebaseObject, $firebaseArray, $sce) {
+	.controller("AppCtrl", function($scope, $firebaseObject, $firebaseArray, $sce, District) {
 		var ref = firebase.database().ref("profiles");
 		$scope.people = $firebaseArray(ref);
 		$scope.people.$loaded().then(function() {
@@ -12,9 +14,10 @@ angular.module("civicsocial", ["firebase"])
 				document.getElementById('currentTwitter'), {
 					height: '500',
 					related: 'twitterdev,twitterapi'
-				});      var url  = "https://www.facebook.com/plugins/page.php?href=" + $scope.current.facebook + "&tabs=timeline&width=300&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=1732805993602261";
-              $scope.current.facebookURL = $sce.trustAsResourceUrl(url);
-              $scope.current.facebook = $sce.trustAsResourceUrl($scope.current.facebook);
+				});
+			var url = "https://www.facebook.com/plugins/page.php?href=" + $scope.current.facebook + "&tabs=timeline&width=300&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=1732805993602261";
+			$scope.current.facebookURL = $sce.trustAsResourceUrl(url);
+			$scope.current.facebook = $sce.trustAsResourceUrl($scope.current.facebook);
 		});
 		$scope.setActive = function(id) {
 			document.getElementById('currentTwitter').innerHTML = "";
@@ -30,12 +33,63 @@ angular.module("civicsocial", ["firebase"])
 						height: '500',
 						related: 'twitterdev,twitterapi'
 					});
+			} else {
+				document.getElementById('currentTwitter').innerHTML = "No Twitter timeline found.";
 			}
-      else {
-        document.getElementById('currentTwitter').innerHTML = "No Twitter timeline found.";
-      }
-      var url  = "https://www.facebook.com/plugins/page.php?href=" + $scope.current.facebook + "&tabs=timeline&width=300&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=1732805993602261";
-      $scope.current.facebookURL = $sce.trustAsResourceUrl(url);
-      $scope.current.facebook = $sce.trustAsResourceUrl($scope.current.facebook);
+			var url = "https://www.facebook.com/plugins/page.php?href=" + $scope.current.facebook + "&tabs=timeline&width=300&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=1732805993602261";
+			$scope.current.facebookURL = $sce.trustAsResourceUrl(url);
+			$scope.current.facebook = $sce.trustAsResourceUrl($scope.current.facebook);
+			$scope.getDisctrict = function() {
+				District.get($scope.address);
+			};
 		};
+	}).factory("District", function() {
+		var output;
+		// Load in which-polygon module for finding district from point
+
+		// Promise that loads in geojson and produces query function
+		var districtQuery = new Promise(function(resolve, reject) {
+			$.getJSON("./assets/geojson/City_Council_Districts.geojson", function(data) {
+				var query = whichPolygon(data);
+				resolve(query);
+			});
+		});
+
+		// ****** TEST ******* //
+		output.get = function() {
+			getCoords(address)
+				.then(function(c) {
+					return getDistrict(c);
+				})
+				.then(function(d) {
+					alert("District for " + address + " is " + d);
+				});
+		};
+
+
+		// ****** GEOCODING FUNCTIONS ******* //
+		// Function for getting district from coordinates;
+		function getDistrict(coords) {
+			return Promise.resolve(
+				districtQuery
+				.then(function(query) {
+					return query(coords).DISTRICT;
+				})
+			);
+		}
+
+		// Function for getting coordinates from an inputted address
+		function getCoords(address) {
+			return new Promise(function(resolve, reject) {
+				var mapboxAccessToken = "pk.eyJ1Ijoic2tva2VuZXMiLCJhIjoiMjA0ZjBhMmQxM2VlOTk4Nzg4ZGNkZTg4ZGEzMzVlMmIifQ.KLx_nUNkguWjPm6v176iVQ";
+				var url = 'https://api.tiles.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=' + mapboxAccessToken;
+
+				$.get(url, function(data) {
+					// Return first entry for now; In future improve to return full list, with user clicking on correct address
+					resolve(data.features[0].geometry.coordinates);
+				});
+			});
+
+		}
+		return output;
 	});
